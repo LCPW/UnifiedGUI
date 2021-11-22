31df936d53e51dbbd4c80e088f72ea39fc0339ff
Benjamin Schiller <benjamin.bs.schiller@fau.de>
2021-11-22 19:56:15 +0100

    refactor question box

--- Views/MessageBoxes.py
+++ Views/MessageBoxes.py
@@ -19,3 +19,20 @@ def warning(style, title, text, informative_text=None):
         return True
     else:
         return False
+
+
+def question(style, title, text, informative_text=None):
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Question)
+    icon_msg = style.standardIcon(getattr(QStyle, 'SP_MessageBoxQuestion'))
+    msg.setWindowIcon(icon_msg)
+    msg.setWindowTitle(title)
+    msg.setText(text)
+    if informative_text:
+        msg.setInformativeText(informative_text)
+    msg.setStandardButtons(QMessageBox.Yes | QMessageBox.Cancel)
+    ret = msg.exec()
+    if ret == QMessageBox.Yes:
+        return True
+    else:
+        return False
