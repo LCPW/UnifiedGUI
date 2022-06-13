2beb69a809d40cc54a7c5d5bf440e74fc0c8ae9a
Steve <steve.kungu@fau.de>
2022-06-13 17:50:05 +0200

    Encoder Update

--- Models/Implementations/Encoders/BartelsEncoder.py
+++ Models/Implementations/Encoders/BartelsEncoder.py
@@ -15,46 +15,72 @@ class BartelsEncoder(EncoderInterface):
         self.voltage = parameter_values['Voltage [V]']
         self.frequency = parameter_values['Frequency [Hz]']
         self.channel = parameter_values['Channel']
-        self.mode = parameter_values['Mode']
+        #self.translation = parameter_values['Translation']
         self.modulation = parameter_values['Modulation']
         self.sleep_time = parameter_values['Sleep time [s]']
-
+        self.timersymbol = 0
+        self.timer = 0
+        self.nextsymbol = 0
+        self.timestart = 10
+        self.timezero = time.time()
         super().setup()
 
     def encode(self, sequence):
         symbol_values = []
 
         numascii = [ord(i) for i in sequence]
-        asciilist = [str.encode(str(i)).decode("ascii") for i in numascii]
-        binarylist = [format(i, "07b") for i in numascii]
 
-        sequence = sequence.replace(",", " ")
-        sequence = sequence.replace(";", " ")
-        wordlist = sequence.split()
+        # if self.translation == "word":
+        #
+        #     sequence = sequence.replace(",", " ")
+        #     sequence = sequence.replace(";", " ")
+        #     wordlist = sequence.split()
+        #
+        #     for i in range(len(wordlist)):
+        #         symbol_values.append(str(wordlist[i]))
+        #     symbol_values.append("-1")
+        #     return symbol_values
+        #
+        # if self.translation == "ascii":
+        #
+        #     asciilist = [str.encode(str(i)).decode("ascii") for i in numascii]
+        #
+        #     for i in range(len(asciilist)):
+        #         symbol_values.append(str(asciilist[i]))
+        #     symbol_values.append("-")
+        #     return symbol_values
 
-        if self.mode == "word":
-            for i in range(len(wordlist)):
-                symbol_values.append(str(wordlist[i]))
-            symbol_values.append("-1")
-            return symbol_values
+        if self.modulation == "2-ASK":
 
-        elif self.mode == "ascii":
-            for i in range(len(asciilist)):
-                symbol_values.append(str(asciilist[i]))
-            symbol_values.append("-")
-            return symbol_values
+            binarylist = [format(i, "07b") for i in numascii]
 
-        elif self.mode == "binary":
             for x in range(len(binarylist)):
                 binaryindex = [int(a) for a in str(binarylist[x])]
                 for i in range(len(binaryindex)):
                     symbol_values.append(str(binaryindex[i]))
                 symbol_values.append("-")
+            self.timezero = time.time()
+            self.nextsymbol = 0
+            return symbol_values
+
+        if self.modulation == "4-ASK":
+
+            binarylist = [format(i, "08b") for i in numascii]
+            ask4list = []
+
+            for x in range(len(binarylist)):
+                binaryindex = [int(a) for a in str(binarylist[x])]
+                for i in range(len(binaryindex)):
+                    ask4list.append(str(binaryindex[i]))
+                    if i % 2 == 1:
+                        symbol_values.append(str((ask4list[i - 1]) + (ask4list[i])))
+                ask4list.clear()
+                symbol_values.append("-")
             return symbol_values
 
     def parameters_edited(self):
 
-        self.mode = self.parameter_values['Mode']
+        #self.translation = self.parameter_values['Translation']
         self.channel = self.parameter_values['Channel']
         self.frequency = self.parameter_values['Frequency [Hz]']
         self.voltage = self.parameter_values['Voltage [V]']
@@ -63,6 +89,8 @@ class BartelsEncoder(EncoderInterface):
         self.modulation = self.parameter_values['Modulation']
 
     def transmit_single_symbol_value(self, symbol_value):
+        ds = 0.5
+        wait = 0.5
 
         ################## MANUALLY ######################
         if self.modulation == "manually":
@@ -73,42 +101,94 @@ class BartelsEncoder(EncoderInterface):
             print(self.bartels.smp.readline().decode("ascii"))
 
         ############### AUTOMATICALLY 2-ASK ####################
-        elif self.modulation == "2-ASK":
+        if self.modulation == "2-ASK":
+
+            while (time.time() - self.timezero) <= self.nextsymbol:
+                ""
 
             if symbol_value != " -":
-                self.transmitters[0].value = int(symbol_value)
+                self.timer = (self.timer + 1) % 7
+                #print(str(symbol_value) + "Verspätung:" + str(round((time.time()-self.timezero-self.nextsymbol)*1000,0)))
+                print(str(self.timer) + ". gesendet: " + str(symbol_value) + "  " + str(time.time() - self.timezero))
+                #self.transmitters[0].value = int(symbol_value)
                 self.bartels.smp.write(b"P" + str.encode(str(self.channel)) +
                                        b"V" + str.encode(str(int(symbol_value) * self.voltage)) +
                                        b"b\r\n")
                 self.bartels.smp.readline().decode("ascii")
+                self.nextsymbol = round(self.nextsymbol + ds,4)
+
+                if self.timer == 0:
+                    self.nextsymbol = round(self.nextsymbol + wait, 4)
+                    time.sleep(wait/2)
+
+            if symbol_value == " -":
+                print("-. gesendet: " + str(symbol_value) + "  " + str(time.time() - self.timezero))
+                self.bartels.smp.write(b"P" + str.encode(str(self.channel)) +
+                                       b"V0" +  # str.encode(str(int(symbol_value) * self.voltage)) +
+                                       b"b\r\n")
+                self.bartels.smp.readline().decode("ascii")
+                self.nextsymbol = round(self.nextsymbol + ds * 2, 4)
+                print(" \r\n")
+
+
+
+
+        ############### AUTOMATICALLY 4-ASK ####################
+        if self.modulation == "4-ASK":
+
+            if symbol_value != " -":
+                self.transmitters[0].value = int(symbol_value)
+
+                if symbol_value == " 00":
+                    self.bartels.smp.write(b"P" + str.encode(str(self.channel)) + b"V0b\r\n")
+                    print(self.bartels.smp.readline().decode("ascii"))
+                    #print(timesend * 1000)
+
+                if symbol_value == " 01":
+                    self.bartels.smp.write(b"P" + str.encode(str(self.channel)) + b"V100b\r\n")
+                    print(self.bartels.smp.readline().decode("ascii"))
+                    #print(timesend * 1000)
+
+                if symbol_value == " 10":
+                    self.bartels.smp.write(b"P" + str.encode(str(self.channel)) + b"V175b\r\n")
+                    print(self.bartels.smp.readline().decode("ascii"))
+                    #print(timesend * 1000)
+
+                if symbol_value == " 11":
+                    self.bartels.smp.write(b"P" + str.encode(str(self.channel)) + b"V250b\r\n")
+                    print(self.bartels.smp.readline().decode("ascii"))
+                    #print(timesend * 1000)
+
             else:
                 self.bartels.smp.write(b"P" + str.encode(str(self.channel)) +
-                                       b"V0" + # str.encode(str(int(symbol_value) * self.voltage)) +
+                                       b"V0" +  # str.encode(str(int(symbol_value) * self.voltage)) +
                                        b"b\r\n")
                 self.bartels.smp.readline().decode("ascii")
                 time.sleep(0.1)
 
 
+
+
 def get_parameters():
     parameters = [
-        {
-            'description': "Mode",
-            'dtype': 'item',
-            'default': "binary",
-            'items': ["ascii", "word", "binary"],
-        },
+        # {
+        #     'description': "Translation",
+        #     'dtype': 'item',
+        #     'default': "binary",
+        #     'items': ["ascii", "word", "binary"],
+        # },
 
         {
             'description': "Modulation",
             'dtype': 'item',
-            'default': "4-ASK",
+            'default': "2-ASK",
             'items': ["manually", "2-ASK", "4-ASK"],
         },
 
         {
             'description': "Port",
             'dtype': 'string',
-            'default': "COM",
+            'default': "COM15",
             'max_length': 5,
         },
 
@@ -123,7 +203,7 @@ def get_parameters():
             'description': "Voltage [V]",
             'dtype': 'int',
             'decimals': 0,
-            'default': 25,
+            'default': 250,
             'min': 0,
             'max': 250,
         },
@@ -132,8 +212,8 @@ def get_parameters():
             'description': "Frequency [Hz]",
             'dtype': 'int',
             'decimals': 0,
-            'default': 0,
-            'min': 0,
+            'default': 75,
+            'min': 50,
             'max': 850,
         },
 
