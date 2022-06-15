d74422293d8e72e383a60a9d01e380fd8acde561
Steve <steve.kungu@fau.de>
2022-06-15 15:31:27 +0200

    Encoder Update. Achtung auf sleep_time bei 2-ASK genutzt!

--- Models/Implementations/Encoders/BartelsEncoder.py
+++ Models/Implementations/Encoders/BartelsEncoder.py
@@ -15,14 +15,16 @@ class BartelsEncoder(EncoderInterface):
         self.voltage = parameter_values['Voltage [V]']
         self.frequency = parameter_values['Frequency [Hz]']
         self.channel = parameter_values['Channel']
-        #self.translation = parameter_values['Translation']
         self.modulation = parameter_values['Modulation']
         self.sleep_time = parameter_values['Sleep time [s]']
+        self.symbolinterval = parameter_values['Symbolinterval[ms]']/1000
         self.timersymbol = 0
         self.timer = 0
-        self.nextsymbol = 0
+        self.nextsymbol = 0.1
         self.timestart = 10
         self.timezero = time.time()
+        self.korrektur = 0
+        self.symbolint = 0
         super().setup()
 
     def encode(self, sequence):
@@ -76,6 +78,8 @@ class BartelsEncoder(EncoderInterface):
                         symbol_values.append(str((ask4list[i - 1]) + (ask4list[i])))
                 ask4list.clear()
                 symbol_values.append("-")
+                self.timezero = time.time()
+                self.nextsymbol = 0
             return symbol_values
 
     def parameters_edited(self):
@@ -87,10 +91,10 @@ class BartelsEncoder(EncoderInterface):
         self.sleep_time = self.parameter_values['Sleep time [s]']
         self.port = self.parameter_values['Port']
         self.modulation = self.parameter_values['Modulation']
+        self.symbolinterval = self.parameter_values['Symbolinterval[ms]']/1000
 
     def transmit_single_symbol_value(self, symbol_value):
-        ds = 0.5
-        wait = 0.5
+        #start = str(time.time() - self.timezero)
 
         ################## MANUALLY ######################
         if self.modulation == "manually":
@@ -103,35 +107,34 @@ class BartelsEncoder(EncoderInterface):
         ############### AUTOMATICALLY 2-ASK ####################
         if self.modulation == "2-ASK":
 
-            while (time.time() - self.timezero) <= self.nextsymbol:
-                ""
-
             if symbol_value != " -":
-                self.timer = (self.timer + 1) % 7
-                #print(str(symbol_value) + "Verspätung:" + str(round((time.time()-self.timezero-self.nextsymbol)*1000,0)))
+                while (time.time()-self.timezero) < self.nextsymbol:
+                    ""
+
                 print(str(self.timer) + ". gesendet: " + str(symbol_value) + "  " + str(time.time() - self.timezero))
-                #self.transmitters[0].value = int(symbol_value)
                 self.bartels.smp.write(b"P" + str.encode(str(self.channel)) +
                                        b"V" + str.encode(str(int(symbol_value) * self.voltage)) +
-                                       b"b\r\n")
+                                       b"\r\n")
                 self.bartels.smp.readline().decode("ascii")
-                self.nextsymbol = round(self.nextsymbol + ds,4)
-
-                if self.timer == 0:
-                    self.nextsymbol = round(self.nextsymbol + wait, 4)
-                    time.sleep(wait/2)
-
-            if symbol_value == " -":
-                print("-. gesendet: " + str(symbol_value) + "  " + str(time.time() - self.timezero))
-                self.bartels.smp.write(b"P" + str.encode(str(self.channel)) +
-                                       b"V0" +  # str.encode(str(int(symbol_value) * self.voltage)) +
-                                       b"b\r\n")
+                self.bartels.smp.write(b"F" + str.encode(str(self.frequency)) + b"\r\n")
                 self.bartels.smp.readline().decode("ascii")
-                self.nextsymbol = round(self.nextsymbol + ds * 2, 4)
+
+                #self.transmitters[0].value = int(symbol_value)
+                self.nextsymbol = round(self.nextsymbol + self.symbolinterval, 2)
+                self.timer = (self.timer + 1) % 7
+
+            else:
+
+                while (time.time() - self.timezero) <= (self.nextsymbol):
+                    ""
+
+                print("-. ges: " + str(symbol_value) + " " + str(time.time() - self.timezero) + " " + str(self.nextsymbol))
+                self.bartels.smp.write(b"P" + str.encode(str(self.channel)) + b"V0b\r\n")
+                self.bartels.smp.readline().decode("ascii")
+                self.bartels.smp.write(b"F0\r\n")
+                self.bartels.smp.readline().decode("ascii")
+                self.nextsymbol = round(self.nextsymbol + self.symbolinterval + self.sleep_time-1, 2) #1-ssy*7 bei 100ms
                 print(" \r\n")
-
-
-
 
         ############### AUTOMATICALLY 4-ASK ####################
         if self.modulation == "4-ASK":
@@ -140,33 +143,41 @@ class BartelsEncoder(EncoderInterface):
                 self.transmitters[0].value = int(symbol_value)
 
                 if symbol_value == " 00":
-                    self.bartels.smp.write(b"P" + str.encode(str(self.channel)) + b"V0b\r\n")
-                    print(self.bartels.smp.readline().decode("ascii"))
-                    #print(timesend * 1000)
-
+                    self.voltage = 25
                 if symbol_value == " 01":
-                    self.bartels.smp.write(b"P" + str.encode(str(self.channel)) + b"V100b\r\n")
-                    print(self.bartels.smp.readline().decode("ascii"))
-                    #print(timesend * 1000)
-
+                    self.voltage = 100
                 if symbol_value == " 10":
-                    self.bartels.smp.write(b"P" + str.encode(str(self.channel)) + b"V175b\r\n")
-                    print(self.bartels.smp.readline().decode("ascii"))
-                    #print(timesend * 1000)
-
+                    self.voltage = 150
                 if symbol_value == " 11":
-                    self.bartels.smp.write(b"P" + str.encode(str(self.channel)) + b"V250b\r\n")
-                    print(self.bartels.smp.readline().decode("ascii"))
-                    #print(timesend * 1000)
+                    self.voltage = 250
+
+                while (time.time() - self.timezero) <= (self.nextsymbol):
+                    ""
+
+                print(str(self.timer) + ". gesendet: " + str(symbol_value) + "  " + str(time.time() - self.timezero))
+                self.bartels.smp.write(b"P"+str.encode(str(self.channel))+b"V"+str.encode(str(self.voltage))+b"\r\n")
+                self.bartels.smp.readline().decode("ascii")
+                self.nextsymbol = round(self.nextsymbol + self.symbolinterval, 2)
+                self.bartels.smp.write(b"F" + str.encode(str(self.frequency)) + b"\r\n")
+                self.bartels.smp.readline().decode("ascii")
+                self.timer = (self.timer + 1) % 4
 
             else:
-                self.bartels.smp.write(b"P" + str.encode(str(self.channel)) +
-                                       b"V0" +  # str.encode(str(int(symbol_value) * self.voltage)) +
-                                       b"b\r\n")
+
+                while (time.time() - self.timezero) <= (self.nextsymbol):
+                    ""
+
+                print("-. gesendet: " + str(symbol_value) + "  " + str(time.time() - self.timezero))
+                self.bartels.smp.write(b"P" + str.encode(str(self.channel)) + b"V0b\r\n")
                 self.bartels.smp.readline().decode("ascii")
-                time.sleep(0.1)
+                self.bartels.smp.write(b"F50\r\n")
+                self.bartels.smp.readline().decode("ascii")
+                self.nextsymbol = round(self.nextsymbol + self.symbolinterval, 2)
+                print(" \r\n")
 
 
+        #self.korrektur = time.time() - self.timezero - float(start)
+        #print(str(self.korrektur))
 
 
 def get_parameters():
@@ -188,14 +199,14 @@ def get_parameters():
         {
             'description': "Port",
             'dtype': 'string',
-            'default': "COM15",
+            'default': "COM5",
             'max_length': 5,
         },
 
         {
             'description': "Channel",
             'dtype': 'item',
-            'default': "4",
+            'default': "1",
             'items': ["1", "2", "3", "4"],
         },
 
@@ -212,18 +223,29 @@ def get_parameters():
             'description': "Frequency [Hz]",
             'dtype': 'int',
             'decimals': 0,
-            'default': 75,
+            'default': 70,
             'min': 50,
             'max': 850,
         },
 
         {
             'description': "Sleep time [s]",
-            'decimals': 3,
+            'decimals': 1,
             'dtype': 'float',
-            'min': 0,
+            'min': 1,
             'max': 100,
-            'default': 0.0,
+            'default': 1,
+        },
+
+        {
+            'description': "Symbolinterval[ms]",
+            'decimals': 0,
+            'dtype': 'float',
+            'min': 100,
+            'max': 1000,
+            'default': 100,
         }
+
+
     ]
     return parameters
