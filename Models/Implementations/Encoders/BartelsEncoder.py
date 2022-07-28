7c99ddbd71d2a7ff3135c2bea7715dde038a6689
Kungu <steve.kungu@fau.de>
2022-07-28 13:18:48 +0200

    Better separation between Transmitter and Encoder

--- Models/Implementations/Encoders/BartelsEncoder.py
+++ Models/Implementations/Encoders/BartelsEncoder.py
@@ -8,129 +8,221 @@ class BartelsEncoder(EncoderInterface):
 
     def __init__(self, parameters, parameter_values):
         super().__init__(parameters, parameter_values)
-        self.port = parameter_values['Port']
+        self.port = parameter_values['port']
         self.bartels = BartelsTransmitter(self.port)
         self.transmitters = [self.bartels]
         self.allowed_sequence_values = [chr(i) for i in range(0, 128)]
-        self.voltage = parameter_values['Voltage [V]']                          # Set micropump voltage
-        self.frequency = parameter_values['Frequency [Hz]']                     # Set micropump frequency
-        self.channel = parameter_values['Channel']                              # Set Channel 1-4
-        self.modulation = parameter_values['Modulation']                        # 2-ASK (0,1) / 4-ASK (00,01,10,11)
-        self.sleep_time = parameter_values['Sleep time [s]']                    # Waiting time until the next symbol
-        self.symbolinterval = parameter_values['Symbolinterval[ms]'] / 1000     # Waiting time between the bits
-        self.timezero = time.time()                                             # Start system time from 0
-        self.timer = 0                                                          # Only for testing (symbol  number)
-        self.nextsymbol = 0                                                     # Set slots for sending
+        self.voltage = parameter_values['voltage [V]']                          # Set micropump voltage
+        self.frequency = parameter_values['frequency [Hz]']                     # Set micropump frequency
+        self.channel = parameter_values['channel']                              # Set Channel 1-4
+        self.modulation = parameter_values['modulation']                        # 2-ASK (0,1) / 4-ASK (00,01,10,11)
+        # self.sleep_time = parameter_values['character interval [s]']          # Waiting time until next Injection
+        self.symbolInterval = parameter_values['symbol interval [ms]'] / 1000   # Waiting time between Symbols
+        self.startTime = time.time()                                            # Get current system time
+        self.binaryOrder = 0                                                    # Only for testing (symbol  number)
+        self.timeNextInjection = 0                                              # Set slots for sending next Injection
         super().setup()
+
+        """
+        Initialisierung der Mikropumpe
+        Aufgaben: 
+        - Automatische Suche und Einstellung des richtigen Geräts (port)
+        - Übernahme der Anfangsparameter von Spannung, Frequenz, Kanal, Modulation, Symbolinterval
+        - Ermittelt aktuelle Systemlaufzeit für die Genauigkeit
+        """
 
     def encode(self, sequence):
 
         symbol_values = []                                                      # Create list for symbols
-        ask4list = []                                                           # Create list for 4-ASK
-        numascii = [ord(i) for i in sequence]                                   # Change sequence in Unicode
-        binarylist = [format(i, "08b") for i in numascii]                       # Change Unicode in Bitsequence
+        list4csk = []                                                           # Create list for 4-CSK
+        list8csk = []                                                           # Create list for 8-CSK
+        sequenceindecimal = [ord(i) for i in sequence]                          # Change sequence in decimal (ascii)
+        sequencein8bit = [format(i, "08b") for i in sequenceindecimal]          # Change decimal in 8bit sequence
 
-        for x in range(len(binarylist)):                                        # Change Bitsequence in single Bits
-            binaryindex = [int(a) for a in str(binarylist[x])]
-            for i in range(len(binaryindex)):
 
-                if self.modulation == "2-ASK":                                  # Add single Bit to symbol list
+        """
+           Informationskodierung in das Binärsystem.
+           Aufgaben: 
+           - Erstellung jeweilige Binärliste für 4-CSK und 8-CSK
+           - Umwandlung von Zeichen in Dezimalzahlen (ascii)
+           - Umwandlung von Dezimalzahlen in Binärsequenz (ascii)
+           Bsp: A --> 65 --> 01000001 bzw. 
+        """
+
+        for x in range(len(sequencein8bit)):
+            binary8index = [int(a) for a in str(sequencein8bit[x])]             # Change 8bit sequence in single Bits
+
+            for i in range(len(binary8index)):
+                if self.modulation == "OOK":                                    # 1Bit --> 1Symbol
                     if i % 8 == 0:
                         continue
-                    symbol_values.append(str(binaryindex[i]))
+                    symbol_values.append(str(binary8index[i]))
 
-                if self.modulation == "4-ASK":                                  # Add two Bits to symbol list
-                    ask4list.append(str(binaryindex[i]))
+                if self.modulation == "4-CSK":                                  # 2Bit --> 1Symbol
+                    list4csk.append(str(binary8index[i]))
                     if i % 2 == 1:
-                        symbol_values.append(str((ask4list[i - 1]) + (ask4list[i])))
+                        symbol_values.append(str((list4csk[i - 1]) + (list4csk[i])))
 
-            ask4list.clear()
+                if self.modulation == "8-CSK":                              # 3Bit --> 1Symbol
+                    list8csk.append(str(binary8index[i]))
+                    if i % 3 == 1 and i > 1:
+                        symbol_values.append(str((list8csk[i - 2]) + (list8csk[i - 1]) + (list8csk[i])))
+            list4csk.clear()
+            list8csk.clear()
+            # symbol_values.append("-") #Wenn du nach jedem Zeichen ein ein "-" willst, dann einfach das # entfernen
+        symbol_values.append("-") # Behebt den Fehler, dass am noch Etwas gesendet wird. Danke Dir! :-)
+
+        """
+           Modulation der Kodierung.
+           Aufgaben: 
+           - Zusammenfassung von Binärsequenz in Symbole nach jeweiliger Modulation
+           Bsp: OOK:    01000001    --> 0,1,0,0,0,0,0,1
+                4-CSK:  01000001    --> 01,00,00,01
+                8-CSK:  000001      --> 000,001 (weniger Zeichen kodierbar)
+        """
+
+        if self.modulation == "manually":  # Add two Bits to symbol list
+            symbol_values.clear()
+            symbol_values.append(sequence)
+
+        if sequence == "500Symbole":
+            symbol_values.clear()
+            for i in range(500):
+                i += 1
+                symbol_values.append(0)
+                symbol_values.append(1)
+            symbol_values.append("-")
+        if sequence == "100Symbole":
+            symbol_values.clear()
+            for i in range(100):
+                i += 1
+                symbol_values.append(0)
+                symbol_values.append(1)
+            symbol_values.append("-")
+        if sequence == "10Symbole":
+            symbol_values.clear()
+            for i in range(10):
+                i += 1
+                symbol_values.append(0)
+                symbol_values.append(1)
             symbol_values.append("-")
 
-        self.timezero = time.time()                                             # Restart system time
-        self.nextsymbol = 0                                                     # Restart slots for sending
+        """
+           Individuelle Kodierung.
+           Aufgaben: 
+            - Vorgefertige Kodierungsschemen
+        """
+
+        self.timeNextInjection = 1                                              # Restart slots for sending
+        self.bartels.micropump_set_frequency(self.frequency)
+        self.startTime = time.time()                                             # Restart system time
         return symbol_values
+
+    """
+    Vorbereitung Transmit
+    Aufgaben: 
+    - Setzt Zeit für erste Injektion
+    - Stellt Frequenz ein
+    - Übernimmt aktuelle Systemzeit
+    """
 
     def parameters_edited(self):
 
-        self.channel = self.parameter_values['Channel']
-        self.frequency = self.parameter_values['Frequency [Hz]']
-        self.voltage = self.parameter_values['Voltage [V]']
-        self.sleep_time = self.parameter_values['Sleep time [s]']
-        self.port = self.parameter_values['Port']
-        self.modulation = self.parameter_values['Modulation']
-        self.symbolinterval = self.parameter_values['Symbolinterval[ms]'] / 1000
+        self.channel = self.parameter_values['channel']
+        self.frequency = self.parameter_values['frequency [Hz]']
+        self.voltage = self.parameter_values['voltage [V]']
+        # self.sleep_time = self.parameter_values['character interval [s]']
+        self.port = self.parameter_values['port']
+        self.modulation = self.parameter_values['modulation']
+        self.symbolInterval = self.parameter_values['symbol interval [ms]'] / 1000
+
+        """
+        Aktualisierung Parameter
+        Aufgaben: 
+        - Prüft Parameter während der Laufzeit
+        - Aktualisiert Parameter für die nächsten Übetragungen (außer Frequenz)
+        """
 
     def transmit_single_symbol_value(self, symbol_value):
-        while (time.time() - self.timezero) < self.nextsymbol:                  # ... system time waits until nextsymbol
+        print(time.time() - self.startTime)
+        while (time.time() - self.startTime) < self.timeNextInjection:         # ... system time waits until nextsymbol
             pass
 
-        # MANUALLY #                                                            # only for testing
-        if self.modulation == "manually":
-            self.bartels.micropump(self.channel, self.voltage, self.frequency)  # set voltage and frequency of micropump
-
-        # 2-ASK #
         if symbol_value != " -":                                                # If the Symbol 0 or 1 then ...
+            timing = time.time() - self.startTime  # - self.timeNextInjection
+            # print(timing)
 
-            if self.modulation == "2-ASK":
-                print(str(self.timer) + ". gesendet: " + str(symbol_value) + "  " + str(time.time() - self.timezero))
-                # print system time + Symbol / only for testing
-                self.bartels.micropump(self.channel, int(symbol_value) * self.voltage, self.frequency)
-                self.nextsymbol = round(self.nextsymbol + self.symbolinterval, 1)  # Determine nextsymbol + interval
-                self.timer = (self.timer + 1) % 7                              # Define symbol number / only for testing
+            if self.modulation == "OOK":
+                # print(str(self.binaryOrder) + ". gesendet: " + str(symbol_value) + "  " + str(timing))  # Testing
+                self.bartels.micropump_set_voltage(self.channel, int(symbol_value) * self.voltage)
+                self.timeNextInjection = self.timeNextInjection + self.symbolInterval
+                # print(str(self.timer) + ". gesendet: " + str(symbol_value) + "  " + str(time.time() - self.timezero - timing))
+                self.binaryOrder = (self.binaryOrder + 1) % 7  # Testing
 
-            if self.modulation == "4-ASK":
+            elif self.modulation == "4-CSK":
+                if self.symbolInterval < 250:
+                    self.symbolInterval = 0.250
+                self.bartels.modulation_4CSK(self.channel, symbol_value, self.startTime, self.timeNextInjection)
+                self.timeNextInjection = self.timeNextInjection + self.symbolInterval
+                print(str(self.binaryOrder) + ". gesendet: " + str(symbol_value) + "  " + str(timing))
 
-                if symbol_value == " 00":
-                    self.voltage = 25
-                if symbol_value == " 01":
-                    self.voltage = 100
-                if symbol_value == " 10":
-                    self.voltage = 150
-                if symbol_value == " 11":
-                    self.voltage = 250
+            elif self.modulation == "8-CSK":
+                if self.symbolInterval < 500:
+                    self.symbolInterval = 0.500
+                self.bartels.modulation_8CSK(self.channel, symbol_value, self.startTime, self.timeNextInjection)
+                self.timeNextInjection = self.timeNextInjection + self.symbolInterval
+                print(str(self.binaryOrder) + ". gesendet: " + str(symbol_value) + "  " + str(timing))
 
-                print(str(self.timer) + ". gesendet: " + str(symbol_value) + "  " + str(time.time() - self.timezero))
-                self.bartels.micropump(self.channel, self.voltage, self.frequency)
-                self.nextsymbol = round(self.nextsymbol + self.symbolinterval, 1)
-                self.timer = (self.timer + 1) % 4
+                # MANUALLY #                                                            # only for testing
+            elif self.modulation == "manually":
+                self.bartels.manually(self.channel, self.voltage,  self.frequency, symbol_value)
+                self.timeNextInjection = self.timeNextInjection + self.symbolInterval
+                self.binaryOrder = (self.binaryOrder + 1) % 4
+                print(str(self.binaryOrder) + ". gesendet: " + str(symbol_value) + "  " + str(time.time() - self.startTime))
 
-        else:  # If the Symbol "-" ...
+        else:  # If Symbol "-" ...
+            self.bartels.micropump_set_voltage(self.channel, 0)
+            # self.timeNextInjection = self.timeNextInjection + self.symbolInterval
 
-            self.bartels.micropump(self.channel, 0, 50)
+        self.timeNextInjection = round(self.timeNextInjection, 3)
 
-            if self.symbolinterval == 0.1:
-                self.nextsymbol = round(self.nextsymbol + 1 - self.symbolinterval * 7 + self.sleep_time - 1, 0)
-            else:
-                self.nextsymbol = round(self.nextsymbol + self.symbolinterval + self.sleep_time - 1, 1)
-            print(" \r\n")  # only for testing
+        """"
+        Übermittlung der Daten
+        Aufgaben: 
+        - Prüft Modulationsart
+        - Stellt je nach Modulationsart entsprechende Spannung ein
+        - Setzt Spannungen je nach Symbolintervall. (Bsp. Änderung jede 100ms)
+        Bsp:
+        4-CSK:  Vorherige Kodierung: B --> 01000001 --> 01,00,00,10 
+                Spannungen: 100V,  0V,  0V, 150V
+        """
 
 
 def get_parameters():
     parameters = [
         {
-            'description': "Port",
+            'description': "port",
             'dtype': 'string',
-            'default': "COM15",
+            'default': BartelsTransmitter.micropump_find_ports(self=True),
             'max_length': 5,
         },
 
         {
-            'description': "Modulation",
+            'description': "modulation",
             'dtype': 'item',
-            'default': "2-ASK",
-            'items': ["manually", "2-ASK", "4-ASK"],
+            'default': "OOK",
+            'items': ["manually", "OOK", "4-CSK", "8-CSK"],
         },
 
         {
-            'description': "Channel",
+            'description': "channel",
             'dtype': 'item',
             'default': "1",
             'items': ["1", "2", "3", "4"],
         },
 
         {
-            'description': "Voltage [V]",
+            'description': "voltage [V]",
             'dtype': 'int',
             'decimals': 0,
             'default': 250,
@@ -139,7 +231,7 @@ def get_parameters():
         },
 
         {
-            'description': "Frequency [Hz]",
+            'description': "frequency [Hz]",
             'dtype': 'int',
             'decimals': 0,
             'default': 70,
@@ -147,23 +239,30 @@ def get_parameters():
             'max': 850,
         },
 
-        {
-            'description': "Sleep time [s]",
-            'decimals': 1,
-            'dtype': 'float',
-            'min': 1,
-            'max': 100,
-            'default': 1,
-        },
+        # {
+        #     'description': "character interval [s]",
+        #     'decimals': 1,
+        #     'dtype': 'int',
+        #     'min': 100,
+        #     'max': 100,
+        #     'default': 0,
+        # },
 
         {
-            'description': "Symbolinterval[ms]",
+            'description': "symbol interval [ms]",
             'decimals': 0,
             'dtype': 'float',
-            'min': 100,
-            'max': 1000,
+            'min': 0,
+            'max': 10000,
             'default': 100,
         }
 
     ]
+
+    """"
+            Parameterauswahl
+            Aufgaben: 
+            - Gibt Parameterauswahl an
+            - Begrenzung einzelner Parameter
+    """
     return parameters
--- Models/Implementations/Transmitters/BartelsTransmitter.py
+++ Models/Implementations/Transmitters/BartelsTransmitter.py
@@ -3,9 +3,6 @@ import serial.tools.list_ports
 
 from Models.Interfaces.TransmitterInterface import TransmitterInterface
 
-timezero = time.time()
-
-
 class BartelsTransmitter(TransmitterInterface):
 
     def __init__(self, port):
@@ -19,6 +16,7 @@ class BartelsTransmitter(TransmitterInterface):
         self.smp.readline().decode("ascii")
         self.smp.write(b"POFF\r\n")
         self.smp.readline().decode("ascii")
+        self.portList = []
 
     def transmit_step(self):
         pass
@@ -26,14 +24,119 @@ class BartelsTransmitter(TransmitterInterface):
     def micropump_set_voltage(self, channel, voltage):
         self.smp.write(b"P" + str.encode(str(channel)) + b"V" + str.encode(str(voltage)) + b"\r\n")
         self.smp.readline().decode("ascii")
+        # print("Channel: " + str(channel) + " Voltage: " + str(voltage))
+        # self.smp.write(b"P" + str.encode(str(channel)) + b"V0b\r\n")
+        # self.smp.readline().decode("ascii")
+        # print("Channel: " + str(channel) + " Voltage: 0")
 
     def micropump_set_frequency(self, frequency):
         self.smp.write(b"F" + str.encode(str(frequency)) + b"\r\n")
-        self.smp.readline().decode("ascii")
+        print(self.smp.readline().decode("ascii"))
 
     def micropump_set_parameters(self, channel, voltage, frequency):
         self.smp.write(b"P" + str.encode(str(channel)) + b"V" + str.encode(str(voltage)) + b"\r\n")
         self.smp.readline().decode("ascii")
         self.smp.write(b"F" + str.encode(str(frequency)) + b"\r\n")
         self.smp.readline().decode("ascii")
-        #return print("Channel: " + str(channel) + " Voltage: " + str(voltage) + " Frequency: " + str(frequency))
+        # return print("Channel: " + str(channel) + " Voltage: " + str(voltage) + " Frequency: " + str(frequency))
+
+    def modulation_4CSK(self, channel, symbol_value, start_time, next_injection_time):
+
+        injection_duration = 0
+        voltage = 225
+        print(next_injection_time)
+        while (time.time() - start_time) < next_injection_time:         # ... system time waits until nextsymbol
+            pass
+
+        timing = time.time() - start_time  # - self.timeNextInjection
+        print(timing)
+
+        if symbol_value == " 00":
+            voltage = 0
+            injection_duration = 0
+        if symbol_value == " 01":
+            injection_duration = 0.100
+        if symbol_value == " 10":
+            injection_duration = 0.150
+        if symbol_value == " 11":
+            injection_duration = 0.200
+
+        self.micropump_set_voltage(channel, voltage)
+
+        start_zero = time.time()
+        while (time.time() - start_zero) < injection_duration:  # ... system time waits until nextsymbol
+            pass
+        self.micropump_set_voltage(channel, 0)
+
+    def modulation_8CSK(self, channel, symbol_value, start_time, next_injection_time):
+
+        injection_duration = 0
+        voltage = 225
+        print(next_injection_time)
+        while (time.time() - start_time) < next_injection_time:         # ... system time waits until nextsymbol
+            pass
+
+        timing = time.time() - start_time  # - self.timeNextInjection
+        print(timing)
+
+        if symbol_value == " 000":  # 00µl/Injection
+            voltage = 0
+            injection_duration = 0
+        if symbol_value == " 001":  # 5µl/Injection
+            injection_duration = 0.050
+        if symbol_value == " 010":  # 10µl/Injection
+            injection_duration = 0.100
+        if symbol_value == " 011":  # 15µl/Injection
+            injection_duration = 0.150
+        if symbol_value == " 100":  # 20µl/Injection
+            injection_duration = 0.200
+        if symbol_value == " 101":  # 25µl/Injection
+            injection_duration = 0.250
+        if symbol_value == " 110":  # 30µl/Injection
+            injection_duration = 0.300
+        if symbol_value == " 111":  # 35µl/Injection
+            injection_duration = 0.350
+
+        self.micropump_set_voltage(channel, voltage)
+
+        start_zero = time.time()
+        while (time.time() - start_zero) < injection_duration:  # ... system time waits until nextsymbol
+            pass
+        self.micropump_set_voltage(channel, 0)
+
+    def manually(self, channel, voltage, frequency, symbol_value):
+
+        if symbol_value == "ON" or symbol_value == "on":
+            self.micropump_set_parameters(channel, voltage, frequency)
+        elif symbol_value == "OFF" or symbol_value == "off":
+            self.micropump_set_parameters(channel, 0, 50)
+        else:
+            self.micropump_set_parameters(channel, voltage, frequency)
+            self.micropump_set_parameters(channel, 0, 50)
+
+    def micropump_find_ports(self):
+        portlist = []
+        findpump = serial.Serial()
+
+        for onePort in serial.tools.list_ports.comports():
+            portlist.append(str(onePort).split(' -'))
+            findpump.port = str(portlist[0][0])
+            if portlist[0][1].__contains__('Seriell' or 'Serial'):
+                if findpump.is_open:
+                    try:
+                        findpump.write(b"SELECTQUADDRIVER\r\n")
+                        if findpump.readline().decode("ascii") == "OK\r\n":
+                            return findpump.port
+                    finally:
+                        print("Port: " + str(findpump.port) + " is checked")
+                else:
+                    try:
+                        findpump.open()
+                        findpump.write(b"SELECTQUADDRIVER\r\n")
+                        if findpump.readline().decode("ascii") == "OK\r\n":
+                            return findpump.port
+                        findpump.close()
+                    finally:
+                        print("Port: " + str(findpump.port) + " is checked")
+            portlist.clear()
+        return "COM"
