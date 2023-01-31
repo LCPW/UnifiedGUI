b6578b689a9899c92a56a19449feaa0b74df119e
Max Bartunik <maxbartunik@gmail.com>
2023-01-31 17:45:39 +0100

    Fix some Fraunhofer stuff

--- Models/Implementations/Encoders/FraunhoferEncoder.py
+++ Models/Implementations/Encoders/FraunhoferEncoder.py
@@ -10,6 +10,7 @@ import serial
 import serial.tools.list_ports
 import graycode
 import math
+import re
 
 from Models.Interfaces.EncoderInterface import EncoderInterface
 from Models.Implementations.Transmitters.FraunhoferTransmitter import FraunhoferTransmitter
@@ -59,7 +60,7 @@ class FraunhoferEncoder(EncoderInterface):
         if is_binary_input:
             binary_sequence = [int(val) for val in sequence]
         else:
-            binary_sequence = BartelsEncoder.bits_from_string(sequence)
+            binary_sequence = FraunhoferEncoder.bits_from_string(sequence)
 
         bit_per_symbol = int(math.log(self.modulation_index, 2))
 
@@ -83,11 +84,11 @@ class FraunhoferEncoder(EncoderInterface):
         - limit injection duration (symbol interval - 25ms)
         - limit modulation index to 8
         """
-        self.frequency = self.parameter_values['frequency [Hz]']
+        self.frequency = int(self.parameter_values['frequency [Hz]'])
         self.voltage = self.parameter_values['voltage [V]']
         self.port = self.parameter_values['port']
         self.modulation = self.parameter_values['modulation']
-        self.modulation_index = self.parameter_values['modulation index']
+        self.modulation_index = int(self.parameter_values['modulation index'])
         self.symbol_interval = self.parameter_values['symbol interval [ms]']
         self.burst_per_val = self.parameter_values['bursts per value']
         self.base_time = self.parameter_values['base time (b) [ms]']
@@ -96,15 +97,15 @@ class FraunhoferEncoder(EncoderInterface):
 
         self.allowed_symbol_values = [str(val) for val in range(0, self.modulation_index)]
 
-        if self.modulation == "CSK" and self.injection_duration*(self.modulation_index-1) > self.symbol_interval:
-            Logging.warning("Nonsensical configuration: Max. injection duration is longer than symbol interval.")
+        if self.modulation == "CSK" and self.burst_per_val*(self.modulation_index-1)*(1/self.frequency) > self.symbol_interval:
+            Logging.warn("Nonsensical configuration: Max. burst duration is longer than symbol interval.")
         
         psk_total_length = self.base_time + (self.modulation_index-1)*self.extra_time
-        if self.modulation == "PSK" and self.injection_duration > psk_total_length:
-            Logging.warning("Nonsensical configuration: Injection duration is longer than symbol interval.")
+        if self.modulation == "PSK" and self.burst_per_val*(1/self.frequency) > psk_total_length:
+            Logging.warn("Nonsensical configuration: Burst duration is longer than symbol interval.")
         
-        if self.modulation == "TSK" and self.injection_duration > self.base_time:
-            Logging.warning("Nonsensical configuration: Injection duration is longer than TSK minimal duration.")
+        if self.modulation == "TSK" and self.burst_per_val*(1/self.frequency) > self.base_time:
+            Logging.warn("Nonsensical configuration: Burst duration is longer than TSK minimal duration.")
 
         if self.modulation == "CSK":
             #CSK uses fixed symbol interval
@@ -158,7 +159,6 @@ class FraunhoferEncoder(EncoderInterface):
         - check modulation index and transmit accordingly
         - This will be called with correct timing
         """
-        symbol_value = str(symbol_value).strip()
 
         injection_bursts = self.burst_per_val
         #For TSK and PSK injection is fixed - go ahead
@@ -174,26 +174,22 @@ class FraunhoferEncoder(EncoderInterface):
     def available_ports():
         ports = serial.tools.list_ports.comports()
     
-        suggested_port = ports[0].name
+        suggested_port = ports[-1].name
         for port in sorted(ports):
-            """
-            conn = None
-            if BartelsTransmitter.HARDWARE_ID in port.hwid:            
-                try:
-                    conn = serial.Serial(port=port.name, baudrate=FraunhoferTransmitter.BAUDRATE, timeout=FraunhoferTransmitter.TIMEOUT)
-                except serial.serialutil.SerialException:
-                    #Port may be in use or wrong
-                    continue
+            conn = None          
+            try:
+                conn = serial.Serial(port=port.name, baudrate=FraunhoferTransmitter.BAUDRATE, timeout=FraunhoferTransmitter.TIMEOUT)
+            except serial.serialutil.SerialException:
+                #Port may be in use or wrong
+                continue
                 
-                conn.write(str.encode("ID\r\n"))
-                read_id = conn.readline().decode('utf-8').replace("\r\n", "")
-                conn.close()
-                if read_id == BartelsTransmitter.DEVICE_ID:
-                    suggested_port = port.name
-                    break
-            """
-            pass
-            #TODO: Implement port suggestion
+            conn.write(str.encode("getInfo\r\n"))
+            read_id = conn.readline().decode('utf-8').replace("\n", "")
+            conn.close()
+            if read_id.startswith("bla"):
+                suggested_port = port.name
+                break
+
             
         return ports, suggested_port
 
