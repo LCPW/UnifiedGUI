3ccaf2f7a80cc1f81c104ac17756e344e603ca9c
maxbartunik <maxbartunik@gmail.com>
2023-02-21 20:56:56 +0100

    Add LDC1614 implementation

--- Models/Implementations/Decoders/LDC1614EVMDecoder.py
+++ Models/Implementations/Decoders/LDC1614EVMDecoder.py
@@ -3,12 +3,13 @@ Author: Benjamin Schiller
 E-mail: benjamin.bs.schiller@fau.de
 """
 import scipy.ndimage
+import serial
+import serial.tools.list_ports
 
 from Models.Interfaces.DecoderInterface import DecoderInterface
 from Models.Implementations.Receivers.LDC1614EVMReceiver import LDC1614EVMReceiver
+from Utils import Logging
 
-
-RECEIVER0_NUM_CHANNELS = 4
 CLK_IN_MHZ = 40.0
 
 
@@ -20,24 +21,8 @@ class LDC1614EVMDecoder(DecoderInterface):
     def __init__(self, parameters, parameter_values):
         super().__init__(parameters, parameter_values)
 
-        self.sigma = parameter_values['Sigma']
-        com_port = parameter_values['COM Port']
-        deglitch_filters = parameter_values["Input Deglitch Filter Bandwidth (MHz)"]
-        settle_counts = [parameter_values["Settle Count CH" + str(c)] for c in range(RECEIVER0_NUM_CHANNELS)]
-        reference_counts = [parameter_values["Reference Count CH" + str(c)] for c in range(RECEIVER0_NUM_CHANNELS)]
-        low_power_activation_mode = parameter_values["Low Power Activation Mode"]
-        drive_current = [parameter_values["Drive Current CH" + str(c)] for c in range(RECEIVER0_NUM_CHANNELS)]
-
-        # Define receivers list
-        self.receivers = [LDC1614EVMReceiver(RECEIVER0_NUM_CHANNELS, CLK_IN_MHZ, com_port, deglitch_filters, settle_counts, reference_counts, low_power_activation_mode, drive_current)]
-
-        self.additional_datalines_names = ["Sensor CH" + str(i) + " Filtered (MHz)" for i in range(RECEIVER0_NUM_CHANNELS)]
-        self.plot_settings = {
-            'additional_datalines_active': [True, True, False, False],
-            'additional_datalines_width': 3,
-            'datalines_active': [[True, True, False, False]],
-            'datalines_width': 3
-        }
+        self.parameter_values = parameter_values
+        self.parameters_edited()
 
         super().setup()
 
@@ -48,7 +33,7 @@ class LDC1614EVMDecoder(DecoderInterface):
         """
         received = self.decoded['received']
         lengths, timestamps = received['lengths'], received['timestamps']
-        for i in range(RECEIVER0_NUM_CHANNELS):
+        for i in range(self.active_channels):
             sensor_values = self.get_received(0, i)
             # No values available
             if sensor_values is None:
@@ -58,64 +43,137 @@ class LDC1614EVMDecoder(DecoderInterface):
             self.additional_datalines[i] = dataline
 
     def parameters_edited(self):
+        
         self.sigma = self.parameter_values['Sigma']
+        
+        port = self.parameter_values['port']
+        deglitch_filter = self.parameter_values["Input Deglitch Filter Bandwidth (MHz)"]
 
+        settle_count = self.parameter_values["Settle Count"]
+        # Settle count allows for the sensor to settle after power up
+        # SETTLECOUNT >= floor((Q_sensor * 40MHz) / (16* f_sensor))
+        # Q_sensor = Rp*sqrt(C/L)
+        # t_settle = Q_sensor / f_sensor = SETTLECOUNT*16/40MHz
+
+        reference_count = self.parameter_values["Reference Count"]
+        # This is an internal meas averaging - the highest possible value is best
+        # R_COUNT = floor(t_count * 40MHz / 16)
+        # t_count = 1/f_sample - t_settle - t_switchdelay ---> trade-off between sample rate (f_sample) and sensitivity
+        # t_switchdelay = 629ns + 5/40MHz = 754ns
+
+        self.active_channels = self.parameter_values["active channels"]
+        self.additional_datalines_names = ["CH" + str(i) + " Filtered (MHz)" for i in range(4)]
+
+
+        t_count = reference_count*16/(CLK_IN_MHZ*1000000)
+        t_settle = settle_count*16/(CLK_IN_MHZ*1000000)
+        t_switchdelay = 0.000000754
+
+        t_sample = (t_count + t_settle + t_switchdelay)*self.active_channels
+        Logging.info(f"Approximate sample rate: {1/t_sample:2.2f} Sa/s")
+
+
+        self.plot_settings = {
+            'additional_datalines_active': [True, self.active_channels>1, self.active_channels>2, self.active_channels>3],
+            'additional_datalines_width': 3,
+            'datalines_active': [[True, self.active_channels>1, self.active_channels>2, self.active_channels>3]],
+            'datalines_width': 3
+        }
+
+        #Clean up
+        self.shutdown()
+
+        # Define receivers list
+        receiver = LDC1614EVMReceiver(self.active_channels, CLK_IN_MHZ, port, deglitch_filter, settle_count, reference_count)
+        self.receivers = [receiver]
+        self.receiver_names = ["LDC1614"]
+
+
+    def available_ports():
+        ports = serial.tools.list_ports.comports()
+    
+        suggested_port = ports[0].name
+        for port in sorted(ports):
+            if LDC1614EVMReceiver.HARDWARE_ID in port.hwid:          
+                try:
+                    serial.Serial(port=port.name, baudrate=LDC1614EVMReceiver.BAUDRATE, timeout=LDC1614EVMReceiver.TIMEOUT)
+                except serial.serialutil.SerialException:
+                    #Port may be in use or wrong
+                    continue
+                
+                suggested_port = port.name
+                break
+            
+        return ports, suggested_port
+
+    def shutdown(self):
+        if self.receivers is not None:
+            for rx in self.receivers:
+                try:
+                    rx.shutdown()
+                except:
+                    pass
+
+    def decoder_started(self):
+        if self.receivers is not None:
+            for rx in self.receivers:
+                try:
+                    rx.set_streaming(True)
+                except:
+                    pass
+    
+    def decoder_stopped(self):
+        if self.receivers is not None:
+            for rx in self.receivers:
+                try:
+                    rx.set_streaming(False)
+                except:
+                    pass
 
     def get_parameters():
+        ports, suggested_port = LDC1614EVMDecoder.available_ports()
+
         parameters = [
             {
-                'description': "COM Port",
+                'description': "port",
+                'dtype': 'item',
+                'default': suggested_port,
+                'items': [port.name for port in ports],
+            },
+            {
+                'description': "active channels",
                 'dtype': 'int',
-                'min': 0,
-                'max': 100,
-                'default': 3,
-                'editable': False
+                'min': 1,
+                'max': 4,
+                'default': 1,
+                'editable': True
             },
             {
                 'description': "Input Deglitch Filter Bandwidth (MHz)",
                 'dtype': 'item',
                 'items': ['1.0', '3.3', '10', '33'],
                 'default': '33',
-                'editable': False
+                'editable': True
             },
             {
-                'description': "Low Power Activation Mode",
-                'dtype': 'bool',
-                'default': False,
-                'editable': False
-            }]
-
-        for c in range(RECEIVER0_NUM_CHANNELS):
-            parameters.append({
-                'description': "Settle Count CH" + str(c),
+                'description': "Settle Count",
                 'dtype': 'int',
-                'min': 0,
+                'min': 2,
                 'max': 65535,
                 'default': 1024,
-                'editable': False,
-                'conversion_function': lambda x: "Settling Time: " + str((x*16)/CLK_IN_MHZ) + "us"
-            })
-
-        for c in range(RECEIVER0_NUM_CHANNELS):
-            parameters.append({
-                'description': "Reference Count CH" + str(c),
+                'editable': True,
+                'conversion_function': lambda x: str((x*16)/CLK_IN_MHZ) + "us"
+            },
+            {
+                'description': "Reference Count",
                 'dtype': 'int',
-                'min': 0,
+                'min': 5,
                 'max': 65535,
                 'default': 65535,
-                'editable': False,
-                'conversion_function': lambda x: "Conversion Time: " + str((x*16 + 4)/CLK_IN_MHZ) + "us"
-            })
+                'editable': True,
+                'conversion_function': lambda x: str((x*16)/CLK_IN_MHZ) + "us"
+            }]
 
-        for c in range(RECEIVER0_NUM_CHANNELS):
-            parameters.append({
-                'description': "Drive Current CH" + str(c),
-                'dtype': 'int',
-                'min': -1,
-                'max': 31,
-                'default': -1,
-                'editable': False
-            })
 
         parameters.append({
                 # Sigma value for Gaussian filter
--- Models/Implementations/Receivers/LDC1614EVMReceiver.py
+++ Models/Implementations/Receivers/LDC1614EVMReceiver.py
@@ -1,19 +1,14 @@
-"""
-Author: Benjamin Schiller
-E-mail: benjamin.bs.schiller@fau.de
-"""
-import sys
 
 # Install 'pyserial' for module 'serial'
 import serial
 import crcmod.predefined
-import binascii
 
 from Models.Interfaces.ReceiverInterface import ReceiverInterface
 from Utils import Logging
 
 
 crc8 = crcmod.predefined.mkCrcFun('crc-8')
+#LDC1614 EVM registers
 DATA_MSB = ['00', '02', '04', '06']
 DATA_LSB = ['01', '03', '05', '07']
 OFFSET = ['0C', '0D', '0E', '0F']
@@ -22,11 +17,30 @@ SETTLE_COUNT = ['10', '11', '12', '13']
 REFERENCE_COUNT = ['08', '09', '0A', '0B']
 DRIVE_CURRENT = ['1E', '1F', '20', '21']
 
-
 DEVICE_ID = '7F'
 CONFIG = '1A'
+ERROR_CONFIG = '19'
 MUX_CONFIG = '1B'
 STATUS = '18'
+
+# FAQ on serial com with LDC16xx EVM
+# https://e2e.ti.com/support/sensors-group/sensors/f/sensors-forum/295036/faq-inductive-sensing-faq---frequently-asked-questions#Q40
+
+WRITE_REG_COMMAND = "4C150100042A" # syntax HEADER XX YYYY ZZ, XX write reg adress, YYYY two bytes to write MSB first, ZZ CRC-8 check sum
+# after write, MCU responds with 32 bytes - byte 4 is zero if command is correct
+
+SET_REG_COMMAND = "4C150100022A" # syntax HEADER XX ZZ, xx reg adress to read, ZZ CRC-8 check sum
+# after set, MCU responds with 32 bytes - byte 4 is zero if command is correct
+
+READ_REG_COMMAND = "4C140100022A02" # syntax HEADER ZZ, ZZ CRC-8 check sum
+# after read, MCU responds with 32 bytes - byte 4 is zero if command is correct, bytes 7 and 8 contain MSB and LSB of read
+
+START_STREAMING_COMMAND = "4C0501000601290404302AC1"
+# after set, MCU responds with 32 bytes - byte 4 is zero if command is correct
+# will periodically output 32 bytes - bytes 7-10 CH0, bytes 11-14 CH1, ... MSB first
+
+STOP_STREAMING_COMMAND = "4C0601000101D2"
+# after set, MCU responds with 32 bytes - byte 4 is zero if command is correct
 
 
 class LDC1614EVMReceiver(ReceiverInterface):
@@ -34,160 +48,204 @@ class LDC1614EVMReceiver(ReceiverInterface):
     Decoder for the Texas Instruments LDC1614 Evaluation Module.
     Datasheet: https://www.ti.com/lit/ds/symlink/ldc1612.pdf
     """
-    def __init__(self, num_channels, clk_in_mhz, com_port, deglitch_filter, settle_counts, reference_counts, low_power_activation_mode, drive_currents):
+
+    HARDWARE_ID = "2047:08F8" #LDC1614 EVM
+    BAUDRATE = 115200
+    TIMEOUT = 0.1 #s
+
+    def __init__(self, num_channels, clk_in_mhz, com_port, deglitch_filter, settle_count, reference_count):
         super().__init__()
 
-        self.num_sensors = num_channels
+        self.num_sensors = 4
+        self.active_channels = num_channels
         self.clk_in_mhz = clk_in_mhz
-        self.sensor_names = ["Sensor CH" + str(i) + " (MHz)" for i in range(self.num_sensors)]
+        self.sensor_names = ["CH" + str(i) + " (MHz)" for i in range(4)]
         self.drop_first_measurements = 5
 
         super().setup()
 
-        self.serial_port = serial.Serial(port='COM' + str(com_port), baudrate=115200)
-        device_id = read_reg(self.serial_port, DEVICE_ID)
-        Logging.info(f"Connected to LDC1614 with device ID 0x{device_id}.")
+        self.serial_port = serial.Serial()
+        self.serial_port.port = com_port
+        self.serial_port.baudrate = LDC1614EVMReceiver.BAUDRATE
+        self.serial_port.timeout = LDC1614EVMReceiver.TIMEOUT
+        if not self.serial_port.is_open:
+            try:
+                self.serial_port.open()
+            except serial.serialutil.SerialException:
+                Logging.error("Cannot connect to selected port.")
+                return
 
-        if deglitch_filter == "1.0":
-            deglitch_filter_bits = '001'
-        elif deglitch_filter == "3.3":
-            deglitch_filter_bits = '100'
-        elif deglitch_filter == "10":
-            deglitch_filter_bits = '101'
+        self.set_error_config()
+        self.set_mux_config(deglitch_filter, num_channels)
+        self.set_settle_count(settle_count)
+        self.set_reference_count(reference_count)
+        self.set_config()
+
+    def set_mux_config(self, deglitch_filter_value, channel_count):
+        filter_set = 0
+        if deglitch_filter_value == "1.0":
+            filter_set = 0b001
+        elif deglitch_filter_value == "3.3":
+            filter_set = 0b100
+        elif deglitch_filter_value == "10":
+            filter_set = 0b101
         else:
-            deglitch_filter_bits = '111'
+            filter_set = 0b111
 
-        mux_config_bits = '1100001000001' + deglitch_filter_bits
-        # Enable/disable Rp override for sensor current drive
-        use_drive_current = False if all(i == -1 for i in drive_currents) else True
-        # Use reference frequency from CLKIN pin (6th bit)
-        config_bits = '000' + ('1' if use_drive_current else '0') + ('1' if low_power_activation_mode else '0') + '01000000000'
-        if use_drive_current:
-            for s in range(self.num_sensors):
-                drive_current_bits = bin(drive_currents[s])[2:].zfill(5) + '00000000000'
-                write_reg(self.serial_port, DRIVE_CURRENT[s], binary_to_hex(drive_current_bits))
+        #We can set single channel or auto channel using CH0 & 1, CH0 & 1 & 2 or CH0 & 1 & 2 & 3
+        autoscan_enable = 0
+        rr_sequence = 0
+        if channel_count > 1:
+            autoscan_enable = 1
+            rr_sequence = channel_count - 2
+        
+        mux_config = 0b0000001000001000
+        mux_config |= autoscan_enable<<15
+        mux_config |= rr_sequence<<13
+        mux_config |= filter_set<<0
 
-        write_reg(self.serial_port, CONFIG, binary_to_hex(config_bits))
-        write_reg(self.serial_port, MUX_CONFIG, binary_to_hex(mux_config_bits))
-        for s in range(self.num_sensors):
-            write_reg(self.serial_port, SETTLE_COUNT[s], binary_to_hex(bin(settle_counts[s])[2:]))
-            write_reg(self.serial_port, REFERENCE_COUNT[s], binary_to_hex(bin(reference_counts[s])[2:]))
+        self.write_register(MUX_CONFIG, f'{mux_config:04x}')
+
+    def set_settle_count(self, settle_count):
+        for ch in range(4):
+            self.write_register(SETTLE_COUNT[ch], f'{settle_count:04x}')
+
+    def set_reference_count(self, r_count):
+        for ch in range(4):
+            self.write_register(REFERENCE_COUNT[ch], f'{r_count:04x}')
+
+    def set_config(self):
+        # Default config
+        config = 0b0000001000000001
+        # CH0 when in single mode, sleep off, no I override, full current sensor activation
+        # auto amplitude correction, external clk, res, use interrupt pin, normal current, resx6
+
+        self.write_register(CONFIG, f'{config:04x}')
+
+    def set_error_config(self):
+        #Report errors to data fields and no error interrupts
+        error_config = 0b1111100000000000
+
+        self.write_register(ERROR_CONFIG, f'{error_config:04x}')
+
+    def set_streaming(self, on):
+        if on:
+            self.send_command(START_STREAMING_COMMAND, False)
+        else:
+            self.send_command(STOP_STREAMING_COMMAND, False)
 
     def listen_step(self):
         """
         Check if data ready flag is set (all channels have an unread conversion) and then read new values.
         """
-        status = hex_to_binary(read_reg(self.serial_port, STATUS))
-        data_ready = status[9]
-        if data_ready == '1':
-            values = []
-            for i in range(self.num_sensors):
-                frequency = self.get_frequency(i)
-                values.append(frequency)
+
+        if self.serial_port.in_waiting >= 32:
+            ch0, ch1, ch2, ch3 = self.read_stream_line()
+
+            values = [ch0]
+            if self.active_channels > 1:
+                values.append(ch1)
+            if self.active_channels > 2:
+                values.append(ch2)
+            if self.active_channels > 3:
+                values.append(ch3)
+         
             self.append_values(values)
 
-    def get_frequency(self, channel):
+    def is_data_ready(self, channel):
+        status = self.read_register(STATUS)
+        return status & (1<<(3-channel)) > 0
+
+    def read_stream_line(self):
+        read = self.serial_port.read(32)
+
+        ch0_msb, ch0_lsb = self.get_data_from_bytes(7, read)
+        ch0 = self.calculate_frequency(ch0_msb, ch0_lsb, 0)
+
+        ch1_msb, ch1_lsb = self.get_data_from_bytes(11, read)
+        ch1 = self.calculate_frequency(ch1_msb, ch1_lsb, 1)
+
+        ch2_msb, ch2_lsb = self.get_data_from_bytes(15, read)
+        ch2 = self.calculate_frequency(ch2_msb, ch2_lsb, 2)
+
+        ch3_msb, ch3_lsb = self.get_data_from_bytes(19, read)
+        ch3 = self.calculate_frequency(ch3_msb, ch3_lsb, 3)
+
+        return ch0, ch1, ch2, ch3
+
+    def get_data_from_bytes(self, start_pos, raw):
+        msb = (raw[start_pos]<<8)+raw[start_pos+1]
+        lsb = (raw[start_pos+2]<<8)+raw[start_pos+3]
+
+        return msb, lsb
+
+    def calculate_frequency(self, data_msb, data_lsb, channel):
+        # The leading 4 bits of the MSB are error bits and not part of the actual data
+
+        tes = data_msb & (1<<15)
+        if data_msb & (1<<15) > 0 and channel < self.active_channels:
+            Logging.warning(f"ERR_UR{channel}: Channel {channel} Conversion Under-range Error", repeat=False)
+        if data_msb & (1<<14) > 0 and channel < self.active_channels:
+            Logging.warning(f"ERR_OR{channel}: Channel {channel} Conversion Over-range Error", repeat=False)
+        if data_msb & (1<<13) > 0 and channel < self.active_channels:
+            Logging.warning(f"ERR_WD{channel}: Channel {channel} Conversion Watchdog Timeout Error", repeat=False)
+        if data_msb & (1<<12) > 0 and channel < self.active_channels:
+            Logging.warning(f"ERR_AE{channel}: Channel {channel} Conversion Amplitude Error", repeat=False)
+        
+        #Clear error bits for value calculation
+        data_msb &= 0b0000111111111111
+
+        # Combine values from MSB and LSB to final value
+        data = (data_msb<<16) + data_lsb
+
+        # Calculate reference frequency and channel offset
+        offset = 0 #we currently do not use an offset or dividers
+        reference_divider = 1
+        input_divider = 1
+        reference_frequency = self.clk_in_mhz * 1000000 / reference_divider
+        channel_offset = (offset / 2 ** 16) * reference_frequency
+
+        # Calculate frequency (see page 39 of the data sheet)
+        frequency = input_divider * reference_frequency * ((data / 2 ** 28) + (channel_offset / 2 ** 16))
+        return frequency
+
+    def get_channel_frequency(self, channel):
         """
         Reads the data registers and calculates the frequency for a given channel.
         :param channel: Channel to be read.
         :return: Frequency (Hz).
         """
         # Read least and most significant bytes register (2 each)
-        data_lsb = hex_to_binary(read_reg(self.serial_port, DATA_LSB[channel]))
-        data_msb = hex_to_binary(read_reg(self.serial_port, DATA_MSB[channel]))
+        data_lsb = self.read_register(DATA_LSB[channel])
+        data_msb = self.read_register(DATA_MSB[channel])
 
-        # Drop the leading 4 bits of the MSB because these are error bits and not part of the actual data
-        error_bits = data_msb[:4]
-        if error_bits[0] != '0':
-            Logging.warning(f"ERR_UR{channel}: Channel {channel} Conversion Under-range Error")
-        if error_bits[1] != '0':
-            Logging.warning(f"ERR_OR{channel}: Channel {channel} Conversion Over-range Error")
-        if error_bits[2] != '0':
-            Logging.warning(f"ERR_WD{channel}: Channel {channel} Conversion Watchdog Timeout Error")
-        if error_bits[3] != '0':
-            Logging.warning(f"ERR_AE{channel}: Channel {channel} Conversion Amplitude Error")
-        data_msb = data_msb[4:]
+        return self.calculate_frequency(data_msb, data_lsb, channel)
 
-        # Combine values from MSB and LSB to final value after converting them to integers
-        data = int(data_msb, 2) * 2 ** 16 + int(data_lsb, 2)
+    def shutdown(self):
+        self.serial_port.close()
 
-        # Read offset register and convert to integer
-        offset = read_reg(self.serial_port, OFFSET[channel])
-        offset = (int(offset, 16))
+    def send_command(self, command, append_crc = True):
+        out_cmd = bytes.fromhex(command.upper())
+        if append_crc:
+            crc_byte = bytes([crc8(out_cmd)])
+            out_cmd += crc_byte
 
-        # Read clock dividers register, convert to bits, drop the leading b' and fill up with leading zeros if necessary
-        clock_dividers = read_reg(self.serial_port, CLOCK_DIVIDER[channel])
-        clock_dividers = (bin(int(clock_dividers, 16))[2:]).zfill(16)
+        self.serial_port.write(out_cmd)
+        response = self.serial_port.read(32)
+        # byte 4 indicates an error
+        if response[3] != 0:
+            Logging.warning("Error setting command " + command)
 
-        # The first 10 bits represent the channel reference divider, the last 4 bits represent the channel input divider
-        reference_divider = int(clock_dividers[6:], 2)
-        input_divider = int(clock_dividers[:4], 2)
+        return response
 
-        # Calculate reference frequency and channel offset
-        reference_frequency = self.clk_in_mhz / reference_divider
-        channel_offset = (offset / 2 ** 16) * reference_frequency
+    def write_register(self, register, data):
+        self.send_command(WRITE_REG_COMMAND + register + data)
 
-        # Calculate frequency (see page 39 of the data sheet)
-        frequency = input_divider * reference_frequency * ((data / 2 ** 28) + (channel_offset / 2 ** 16))
+    def read_register(self, register):
+        self.send_command(SET_REG_COMMAND + register)
+        response = self.send_command(READ_REG_COMMAND)
+        resp_msb = response[6]
+        resp_lsb = response[7]
 
-        return frequency
-
-
-def write_reg(serial_port, addr, data):
-    """
-    Writes on the serial connection
-    :param serial_port: Serial port.
-    :param addr: Address.
-    :param data: Data.
-    """
-    serial_string = '4C150100042A' + addr + data
-    serial_bytes = bytes.fromhex(serial_string)
-    crc_byte = bytes([crc8(serial_bytes)])
-    serial_port.write(serial_bytes + crc_byte)
-    s = serial_port.read(32)
-    if s[3] != 0:
-        Logging.warning("Error in write register")
-
-
-def read_reg(serial_port, addr):
-    """
-    Reads from the serial connection.
-    :param serial_port: Serial port.
-    :param addr: Address.
-    :return: Read data (2 bytes) in hex representation.
-    """
-    serial_string = '4C150100022A' + addr
-    serial_bytes = bytes.fromhex(serial_string)
-    crc_byte = bytes([crc8(serial_bytes)])
-    serial_port.write(serial_bytes + crc_byte)
-    s = serial_port.read(32)
-    if s[3] != 0:
-        Logging.warning("Error in set register")
-    serial_string = '4C140100022A02'
-    serial_bytes = bytes.fromhex(serial_string)
-    crc_byte = bytes([crc8(serial_bytes)])
-    serial_port.write(serial_bytes + crc_byte)
-    s = serial_port.read(32)
-    if s[3] != 0:
-        Logging.warning("Error in set register")
-    data_read = (str(hex(s[6]))[2:]).zfill(2) + (str(hex(s[7]))[2:]).zfill(2)
-    return data_read
-
-
-def binary_to_hex(binary):
-    """
-    Converts a binary string to 4-symbol hex value.
-    :param binary: Binary string.
-    :return: Hex value.
-    """
-    return (hex(int(binary, 2))[2:]).zfill(4)
-
-
-def hex_to_binary(hex_string):
-    """
-    Converts hex number to binary (16-bit).
-    Convert to binary, drop the leading b' and fill up with leading zeros if necessary
-    :param hex_string: Hex string.
-    :return: Binary string.
-    """
-    return (bin(int(hex_string, 16))[2:]).zfill(16)
+        val = (resp_msb<<16) + resp_lsb
+        return val
