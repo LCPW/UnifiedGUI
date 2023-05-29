dc23f57b0d5d355315c1dd694edc69488ea4f59c
Max Bartunik <maxbartunik@gmail.com>
2023-05-29 13:41:34 +0200

    Added Pocket LoC sensor

---
+++ Models/Implementations/Decoders/PocketLoCDecoder.py
@@ -0,0 +1,189 @@
+
+import scipy.ndimage
+import serial
+import serial.tools.list_ports
+import numpy as np
+
+from Models.Interfaces.DecoderInterface import DecoderInterface
+from Models.Implementations.Receivers.PocketLoCReceiver import PocketLoCReceiver
+from Utils import Logging
+
+
+class PocketLoCDecoder(DecoderInterface):
+    """
+    Decoder for the Texas Instruments LDC1614 Evaluation Module.
+    Datasheet: https://www.ti.com/lit/ds/symlink/ldc1612.pdf
+    """
+    def __init__(self, parameters, parameter_values):
+        super().__init__(parameters, parameter_values)
+
+        self.parameter_values = parameter_values
+        self.parameters_edited()
+
+        super().setup()
+
+    def calculate_additional_datalines(self):
+        """
+        Generates Gaussian filtered version of the datalines.
+        Note: Can be optimized by avoiding re-calculating old values every time.
+        """
+        received = self.decoded['received']
+        lengths, timestamps = received['lengths'], received['timestamps']
+        for i in range(self.active_channels):
+            sensor_values = self.get_received(0, i)
+            # No values available
+            if sensor_values is None:
+                return
+            sensor_filtered = scipy.ndimage.gaussian_filter1d(sensor_values, self.sigma)
+            dataline = {'length': lengths[0], 'timestamps': timestamps[0][:lengths[0]], 'values': sensor_filtered}
+            self.additional_datalines[i] = dataline
+
+    def parameters_edited(self):
+        
+        
+        port = self.parameter_values['Port']
+        gain = self.parameter_values["Gain"]
+        sample_time = self.parameter_values["Sample duration"]
+
+        sensor1 = self.parameter_values["Sensor 1"]
+        sensor2 = self.parameter_values["Sensor 2"]
+        sensor3 = self.parameter_values["Sensor 3"]
+        sensor4 = self.parameter_values["Sensor 4"]
+        sensor5 = self.parameter_values["Sensor 5"]
+        sensor6 = self.parameter_values["Sensor 6"]
+
+
+        self.plot_settings = {
+            'additional_datalines_active': [],
+            'additional_datalines_width': 0,
+            'datalines_active': [[True, True, True, True, True, True]],
+            'datalines_width': 6
+        }
+
+        #Clean up
+        self.shutdown()
+
+        # Define receivers list
+        receiver = PocketLoCReceiver(port, [sensor1, sensor2, sensor3, sensor4, sensor5, sensor6])
+        receiver.set_gain(gain)
+        receiver.set_sample_time(sample_time)
+
+        self.receivers = [receiver]
+        self.receiver_names = ["Colour Sensor"]
+
+    def clear(self):
+        return super().clear()
+
+    def available_ports():
+        ports = serial.tools.list_ports.comports()
+    
+        suggested_port = ports[0].name
+        for port in sorted(ports):
+            if PocketLoCReceiver.HARDWARE_ID in port.hwid:          
+                try:
+                    serial.Serial(port=port.name, baudrate=PocketLoCReceiver.BAUDRATE, timeout=PocketLoCReceiver.TIMEOUT)
+                except serial.serialutil.SerialException:
+                    #Port may be in use or wrong
+                    continue
+                
+                suggested_port = port.name
+                break
+            
+        return ports, suggested_port
+
+    def shutdown(self):
+        if self.receivers is not None:
+            for rx in self.receivers:
+                try:
+                    rx.shutdown()
+                except:
+                    pass
+
+    def decoder_started(self):
+        if self.receivers is not None:
+            for rx in self.receivers:
+                try:
+                    rx.set_status(True)
+                except:
+                    pass
+    
+    def decoder_stopped(self):
+        if self.receivers is not None:
+            for rx in self.receivers:
+                try:
+                    rx.set_status(False)
+                except:
+                    pass
+
+    def get_parameters():
+        ports, suggested_port = PocketLoCDecoder.available_ports()
+
+        diode_list = ["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "CLEAR", "NIR"]
+
+        parameters = [
+            {
+                'description': "Port",
+                'dtype': 'item',
+                'default': suggested_port,
+                'items': [port.name for port in ports],
+            },
+            {
+                'description': "Gain",
+                'dtype': 'item',
+                'items': ['0.5x', '1x', '2x', '4x', '8x', '16x', '32x', '64x', '128x', '256x', '512x'],
+                'default': '256x',
+                'editable': True
+            },
+            {
+                'description': "Sample duration",
+                'dtype': 'int',
+                'min': 1,
+                'max': 1000,
+                'default': 10,
+                'editable': True,
+                'conversion_function': lambda x: str(1000/(x*4)) + "Sa/s"
+            },
+            {
+                'description': "Sensor 1",
+                'dtype': 'item',
+                'items': diode_list,
+                'default': "F1",
+                'editable': True
+            },
+            {
+                'description': "Sensor 2",
+                'dtype': 'item',
+                'items': diode_list,
+                'default': "F3",
+                'editable': True
+            },
+            {
+                'description': "Sensor 3",
+                'dtype': 'item',
+                'items': diode_list,
+                'default': "F5",
+                'editable': True
+            },
+            {
+                'description': "Sensor 4",
+                'dtype': 'item',
+                'items': diode_list,
+                'default': "F7",
+                'editable': True
+            },
+            {
+                'description': "Sensor 5",
+                'dtype': 'item',
+                'items': diode_list,
+                'default': "F8",
+                'editable': True
+            },
+            {
+                'description': "Sensor 6",
+                'dtype': 'item',
+                'items': diode_list,
+                'default': "NIR",
+                'editable': True
+            }]
+
+        return parameters
---
+++ Models/Implementations/Receivers/PocketLoCReceiver.py
@@ -0,0 +1,145 @@
+
+
+from Models.Interfaces.ReceiverInterface import ReceiverInterface
+from Utils import Logging
+
+import serial
+import time
+import re
+
+class PocketLoCReceiver(ReceiverInterface):
+
+    HARDWARE_ID = "2341:8037"
+    DEVICE_ID = "PocketLoCSensor"
+    BAUDRATE = 9600
+    TIMEOUT = 0.2 #s
+
+    def __init__(self, port, active_sensor_channels):
+        super().__init__()
+
+        self.num_sensors = len(active_sensor_channels)
+        self.sensor_names = [active_sensor_channels]
+
+        self.smp = serial.Serial()
+        self.smp.port = str(port)
+        self.smp.baudrate = PocketLoCReceiver.BAUDRATE
+        self.smp.timeout = PocketLoCReceiver.TIMEOUT
+        if not self.smp.is_open:
+            try:
+                self.smp.open()
+            except serial.serialutil.SerialException:
+                Logging.error("Cannot connect to selected port.")
+                return
+
+        self.smp.write(str.encode("ID\r\n"))
+        read_id = self.smp.readline().decode('utf-8').replace("\r\n", "")
+        if read_id != PocketLoCReceiver.DEVICE_ID:
+            Logging.warning("Unexpected device ID. Are you sure you are connected to the correct port?")
+
+        self.set_mux(active_sensor_channels)
+
+        super().setup()
+
+    def send_command(self, command):
+        #send a command on the active serial connection
+        
+        byte_cmd = str.encode(command + "\r\n")
+        self.smp.write(byte_cmd)
+
+    def read_response(self, n):
+        # Get n lines of data from the serial connection and strip line terminator
+        for i in range(n):
+            raw = self.smp.readline().decode('utf-8')
+            Logging.info(raw.replace("\r\n", ""))
+
+    def set_gain(self, gain_level):
+        # Gain level can be 0...10
+        gain_cmd = "G" + str(gain_level)
+        self.send_command(gain_cmd)
+        self.read_response(1)
+
+    def set_mux(self, active_list):
+        #Try to create the mux command for the sensor depending on a string array of actviated photodiodes
+        #Max. 6 photodiodes can be activated at once
+        #Accepted string inputs are F1, F2, F3, F4, F5, F6, F7, F8, CLEAR, NIR
+        
+        diode_names = ["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "CLEAR", "NIR"]
+        
+        output_mux = [0] * 10
+        
+        for diode in active_list:
+            try:
+                idx = diode_names.index(diode)
+                output_mux[idx] = 1
+            except ValueError:
+                Logging.error("The specified photodiode does not exist. Check your config. Results may contain unexpected values.")
+        
+        if sum(output_mux) > 6:
+            Logging.error("You have selected to many photodiodes. Check your config. Only the first 6 diodes will actually be set.")
+        
+        command = "C" + "".join(str(x) for x in output_mux)
+
+        self.send_command(command)
+        self.read_response(3)
+
+    def set_sample_time(self, sample_duration):
+        # sample duration in ms - good values are 5~10ms
+        time_command = "T" + str(sample_duration)
+        self.send_command(time_command)
+        self.read_response(1)
+
+    def read_values(self):
+        # Get a line of data from the serial connection and parse it
+        raw = self.smp.readline().decode('utf-8')
+        raw_items = re.split("\\t", raw)
+        
+        sensor0_vals = [0,0,0,0,0,0]
+        sensor1_vals = [0,0,0,0,0,0]
+        saturation_error = False
+        timestamp = time.time()
+        
+        try:
+            values = [float(x) for x in raw_items[0:14]]
+            sensor0_vals = values[0:6]
+            sensor1_vals = values[7:13]
+            saturation_error = values[6] > 0 or values[13] > 0
+        except Exception:
+            Logging.warning("Error parsing values:")
+            Logging.warning(raw)
+            return None, None, None, None
+        
+        global error_flag
+        
+        if saturation_error:
+            #Saturation error - only report if new
+            if not error_flag:
+                error_flag = True
+                Logging.warning("Sensor saturation error! You should probably reduce the gain.")
+        else:
+            error_flag = False
+
+        
+        return sensor0_vals, sensor1_vals, saturation_error, timestamp
+
+    
+    def set_status(self, on):
+        write_cmd = b"STOP"
+        if on:
+            write_cmd = b"START"
+
+        self.send_command(write_cmd)
+        self.read_response(1)
+        
+    def shutdown(self):
+        self.set_status(False)
+        self.smp.close()
+
+
+    def listen_step(self):
+        
+        if self.smp.in_waiting > 0:
+            sensor0, sensor1, sat_err, timestamp = self.read_values()
+            if sensor0 is None:
+                return
+
+            self.append_values(sensor0 + sensor1, timestamp)
